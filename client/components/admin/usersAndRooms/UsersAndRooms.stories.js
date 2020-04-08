de0debf315e1b9acbe9c1187c72fb3c165149213
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-04-08 12:24:52 -0300

    Initial commit

--- client/components/admin/usersAndRooms/UsersAndRooms.stories.js
+++ client/components/admin/usersAndRooms/UsersAndRooms.stories.js
@@ -1,25 +1,43 @@
 import React from 'react';
 
+import { ServerContext } from '../../../contexts/ServerContext';
+
 import { UsersAndRooms } from '.';
 
+
 export default {
-	title: 'admin/settings/GroupPage',
+	title: 'admin/pages/UsersAndRooms',
 	component: UsersAndRooms,
-	// decorators: [
-	// 	(storyFn) => <SettingsState>
-	// 		{storyFn()}
-	// 	</SettingsState>,
-	// ],
 };
 
+const usersResult = () => ({
+	result: [1, 2, 3, 4, 5, 6].map((num) => ({
+		_id: num,
+		username: `username - ${ num }`,
+		name: `name - ${ num }`,
+		emails: [{ address: `email - ${ num }` }],
+		roles: [`admin - ${ num }`, `user - ${ num }`],
+		status: `Online - ${ num }`,
+	})),
+	total: 6,
+	success: true,
+});
+
+const roomsResult = () => ({
+	result: [1, 2, 3, 4, 5, 6].map((num) => ({
+		_id: num,
+		name: `name - ${ num }`,
+		users: Math.floor(num * Math.random() * 100),
+		type: 'Channel',
+		messages: Math.floor(num * Math.random() * 1000),
+		default: true,
+		featured: true,
+	})),
+	total: 6,
+	success: true,
+});
+
 export const _default = () =>
-	<UsersAndRooms />;
-
-// export const withGroup = () =>
-// 	<UsersAndRooms
-// 		_id='General'
-// 		i18nLabel='General'
-// 	/>;
-
-// export const skeleton = () =>
-// 	<UsersAndRooms.Skeleton />;
+	<ServerContext.Provider value={{ callEndpoint: async (meth, endpoint, args) => (JSON.parse(args.query).type === 'users' ? usersResult() : roomsResult()) }}>
+		<UsersAndRooms />
+	</ServerContext.Provider>;
--- client/components/admin/usersAndRooms/index.js
+++ client/components/admin/usersAndRooms/index.js
@@ -1,6 +1,37 @@
-import React from 'react';
-import { Box } from '@rocket.chat/fuselage';
+import React, { useState, useCallback, useEffect, useMemo } from 'react';
+import { Avatar, Box, Tabs, Table, TextInput, Tile, Pagination, Icon, Flex, Skeleton } from '@rocket.chat/fuselage';
+
+import { Page } from '../../basic/Page';
+import { useTranslation } from '../../../contexts/TranslationContext';
+import { UsersTab } from './UsersTab';
+import { RoomsTab } from './RoomsTab';
 
 export function UsersAndRooms(props) {
-	return <Box></Box>;
+	const t = useTranslation();
+	const [tab, setTab] = useState('users');
+	return <Avatar.Context.Provider value={{ baseUrl: '/avatar/' }}>
+		<Page>
+			<Page.Header title={t('Users_and_rooms')} />
+			<Tabs>
+				<Tabs.Item selected={tab === 'users'} onClick={() => setTab('users') }>{t('Users')}</Tabs.Item>
+				<Tabs.Item selected={tab === 'rooms'} onClick={() => setTab('rooms') }>{t('Rooms')}</Tabs.Item>
+			</Tabs>
+			<Page.Content>
+				<Box mi='x24'>
+					{
+						(tab === 'users' && <UsersTab />)
+						|| (tab === 'rooms' && <RoomsTab />)
+					}
+				</Box>
+			</Page.Content>
+		</Page>
+	</Avatar.Context.Provider>;
+}
+
+export function GenericTable() {
+	return <Table>
+		<Table.Header>
+
+		</Table.Header>
+	</Table>;
 }
